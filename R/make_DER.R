c31430d68eb13daf5e93fc6368eac5b2a0e09c75
Edward Huang <edwardghg@gmail.com>
2019-07-08 00:36:45 -0700

    Changed coefficients variable name to reduce confusion

--- R/make_DER.R
+++ R/make_DER.R
@@ -24,14 +24,14 @@
 
 make_DER <- function(data, HZE, NTE = TRUE, phi = 2000, y_0 = 0.046404) {
   # Error handling in make_model
-  model <- make_model(data, HZE, NTE, phi, y_0)
+  model      <- make_model(data, HZE, NTE, phi, y_0)
   model_coef <- coef(model)
 
   # Swift light ================================================================
   if (!HZE) {
     # Calibrated Low LET model. Use L = 0, but maybe later will use small L > 0.
-    DER <- function(dose, LET, alph_low = model_coef) {
-      return(1 - exp( - alph_low * dose))
+    DER <- function(dose, LET, coeff = model_coef) {
+      return(1 - exp( - coeff * dose))
     }
 
   # HZE models =================================================================
@@ -39,23 +39,23 @@ make_DER <- function(data, HZE, NTE = TRUE, phi = 2000, y_0 = 0.046404) {
 
     if (NTE) { # HZE NTE -------------------------------------------------------
       # Calibrated hazard function.
-      HZE_NTE_hazard_func <- function(dose, LET, coef) {
-        return(coef[1] * LET * dose * exp( - coef[2] * LET)
-               + (1 - exp( - phi * dose)) * coef[3])
+      HZE_NTE_hazard_func <- function(dose, LET, coeff) {
+        return(coeff[1] * LET * dose * exp( - coeff[2] * LET)
+               + (1 - exp( - phi * dose)) * coeff[3])
       }
       # Calibrated HZE NTE DER.
-      DER <- function(dose, LET, coef = model_coef) {
-        return(1 - exp( - HZE_NTE_hazard_func(dose, LET, coef)))
+      DER <- function(dose, LET, coeff = model_coef) {
+        return(1 - exp( - HZE_NTE_hazard_func(dose, LET, coeff)))
       }
 
     } else if (!NTE) { # HZE TE ------------------------------------------------
       # Calibrated hazard function.
-      HZE_TE_hazard_func <- function(dose, LET, coef) {
-        return(coef[1] * LET * dose * exp( - coef[2] * LET))
+      HZE_TE_hazard_func <- function(dose, LET, coeff) {
+        return(coeff[1] * LET * dose * exp( - coeff[2] * LET))
       }
       # Calibrated HZE TE one-ion DER.
-      DER <- function(dose, LET, coef = model_coef) {
-        return(1 - exp( - HZE_TE_hazard_func(dose, LET, coef)))
+      DER <- function(dose, LET, coeff = model_coef) {
+        return(1 - exp( - HZE_TE_hazard_func(dose, LET, coeff)))
       }
     }
   }
@@ -67,12 +67,12 @@ make_DER <- function(data, HZE, NTE = TRUE, phi = 2000, y_0 = 0.046404) {
 # Hidden functions =============================================================
 
 .low_LET_slope <- function(dose, LET) { # Slope dE/dd of the low LET, low Z model.
-  low_LET_model_coef * exp( - low_LET_model_coef * dose)
+  return(low_LET_model_coef * exp( - low_LET_model_coef * dose))
 }
 
 .HZE_NTE_slope <- function(aa, u, kk1) {
   return((aa + exp( - phi * u) * kk1 * phi) *
-           exp( - (aa * u + (1 -exp( - phi * u)) * kk1)))
+          exp( - (aa * u + (1 -exp( - phi * u)) * kk1)))
 }
 
 .HZE_TE_slope <- function(aa, u, pars = NULL) {
