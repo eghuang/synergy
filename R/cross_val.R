382acd873fe31b1df1200ec6ceed3bd53ae0ebca
Edward Huang <edwardghg@gmail.com>
2019-07-20 16:29:07 -0700

    Refactored code, added necessary arguments, fixed errors in example, added explanatory comments, tested example

--- R/cross_val.R
+++ R/cross_val.R
@@ -1,7 +1,7 @@
-#' @title Applies cross validation to a mixture of ions with respect to
-#'        a synergy theory.
+#' @title Applies leave-one-out cross validation to a mixture of ions with
+#'        respect to the NTE or TE models used from Huang 2019.
 #'
-#' @description Not well tested.
+#' @description
 #'
 #' @param ions List of dataframes corresponding to ion.
 #' @param model String, "NTE" or "TE" for non-targeted or targeted effects.
@@ -13,54 +13,52 @@
 #'
 #' @return Numeric weighted mean errors.
 #'
-#' @details Tested for the two examples below. General correctness is
-#'          unverified and should be treated at some future data.
+#' @details Tested for the two examples below.
 #'
 #' @examples
 #' ##=================== Cross validation ====================#
 #' # Seperate Data into 8 blocks, i.e. test/training sets:
+#' HZE_data <- select(filter(ion_data, Z > 3), 1:length(ion_data))
 #' data_len <- 1:length(HZE_data)
-#' O_350 <- dplyr::select(filter(HZE_data, Beam == "O"), data_len)
-#' #RKS added 3 oxygen points 5/17/2019)
-#' Ne_670 <- dplyr::select(filter(HZE_data, Beam == "Ne"), data_len)
-#' Si_260 <- dplyr::select(filter(HZE_data, Beam == "Si"), data_len)
-#' Ti_1000 <- dplyr::select(filter(HZE_data, Beam == "Ti"), data_len)
-#' Fe_600 <- dplyr::select(filter(HZE_data, LET == 193), data_len)
-#' Fe_350 <- dplyr::select(filter(HZE_data, LET == 253), data_len)
-#' Nb_600 <- dplyr::select(filter(HZE_data, Beam == "Nb"), data_len)
-#' La_593 <- dplyr::select(filter(HZE_data, Beam == "La"), data_len)
+#' O_350 <- select(filter(HZE_data, LET == 20), data_len)
+#' Ne_670 <- select(filter(HZE_data, LET == 25), data_len)
+#' Si_260 <- select(filter(HZE_data, LET == 70), data_len)
+#' Ti_1000 <- select(filter(HZE_data, LET == 100), data_len)
+#' Fe_600 <- select(filter(HZE_data, LET == 193), data_len)
+#' Fe_350 <- select(filter(HZE_data, LET == 253), data_len)
+#' Nb_600 <- select(filter(HZE_data, LET == 464), data_len)
+#' La_593 <- select(filter(HZE_data, LET == 953), data_len)
 #' set_list <- list(O_350, Ne_670, Si_260, Ti_1000,
 #'                  Fe_600, Fe_350, Nb_600, La_593)
 #' actual_prev <- HZE_data$Prev
 #'
 #' NTE_cv <- cross_val(set_list,"NTE", HZE_data$Prev, HZE_data$NWeight)
 #' TE_cv <- cross_val(set_list, "TE", HZE_data$Prev, HZE_data$NWeight)
+#' cv_table <- cbind(NTE_cv, TE_cv)
+#' cv_table
 #'
 #' @author Edward Greg Huang <eghuang@@berkeley.edu>
 #' @export
 
-cross_val <- function(ions, model, prev, w) {
-  theoretical <- vector()
+loo_cv <- function(ions, prev, wgts, model = c("NTE", "TE"),
+                      y_0 = 0.046404, phi = 2000) { # Leave one out
+  pred <- c()
   for (i in 1:length(ions)) {
     test <- ions[[i]]
-    excluded_list <- ions[-i]
-    train <- excluded_list[[1]]
-    for (j in 2:length(excluded_list)) {
-      train <- rbind(train, excluded_list[[j]])
-    }
-    if (model == "NTE") {
-      f <- Prev ~ Y_0 + (1 - exp ( -(aa1 * LET * dose * exp( -aa2 * LET)
+    train <- Reduce(rbind, ions[-i]) # Constructs excluded dataframe
+    if (model == "NTE") { # Fits NTE model
+      f <- Prev ~ y_0 + (1 - exp ( -(aa1 * LET * dose * exp( -aa2 * LET)
                                      + (1 - exp( - phi * dose)) * kk1)))
-      s <- list(aa1 = .00009, aa2 = .001, kk1 = .06)
-    } else {
-      f <- Prev ~ Y_0 + (1 - exp ( -(aate1 * LET * dose * exp( -aate2 * LET))))
-      s <- list(aate1 = .00009, aate2 = .01)
+      init <- list(aa1 = .00009, aa2 = .001, kk1 = .06)
+    } else if (model == "TE") { # Fits TE model
+      f <- Prev ~ y_0 + (1 - exp ( -(aate1 * LET * dose * exp( -aate2 * LET))))
+      init <- list(aate1 = .00009, aate2 = .01)
+    } else { # Error handling
+      stop("Invalid model argument specified.")
     }
-    m <- nls(f, data = train, weights = NWeight, start = s)
-    pred <- predict(m, test)
-    theoretical <- c(theoretical, pred)
+    modl <- nls(f, data = train, weights = NWeight, start = init)
+    pred <- c(pred, predict(modl, test)) # Appends predictions
   }
-  errors <- (theoretical - prev) ^ 2
-  return(weighted.mean(errors, w))
+  errors <- (pred - prev) ^ 2
+  return(weighted.mean(errors, wgts)) # Weighted mean squared error
 }
-
